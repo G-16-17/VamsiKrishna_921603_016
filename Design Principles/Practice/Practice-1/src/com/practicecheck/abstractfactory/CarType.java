271e21f1e1f4e99c3ece256157adf5e8b88b9faa
Vamsi Krishna Rolla <63103226+vamsikrishna1704@users.noreply.github.com>
2021-08-19 09:13:41 +0530

    Add files via upload

---
+++ Design Principles/Practice/Practice-1/src/com/practicecheck/abstractfactory/CarType.java
@@ -0,0 +1,5 @@
+package com.practicecheck.abstractfactory;
+
+public enum CarType {
+	MICRO, MINI, LUXURY
+}
---
+++ Design Principles/Practice/Practice-1/src/com/practicecheck/abstractfactory/Location.java
@@ -0,0 +1,5 @@
+package com.practicecheck.abstractfactory;
+
+public enum Location {
+	DEFAULT, USA, INDIA
+}
